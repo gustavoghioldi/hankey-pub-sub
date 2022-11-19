09b8b2cff2e40ca4f8f9a33379a57391b48390c1
Leandro090685 <lenife@hotmail.com>
2022-11-19 07:13:55 -0300

    signal 2

--- PubSub/publisher/models.py
+++ PubSub/publisher/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.cache import cache
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.forms.models import model_to_dict
 
 class PublisherModel(AbstractUser):
     is_service = models.BooleanField(default=False)
