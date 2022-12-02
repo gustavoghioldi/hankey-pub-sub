3610eba79ae64008013a29fc6acb5a660a040aac
Leandro090685 <lenife@hotmail.com>
2022-12-02 18:24:03 -0300

    Json service

--- PubSub/core/services/http_service.py
+++ PubSub/core/services/http_service.py
@@ -1,1 +1,11 @@
-#
+from django.forms.models import model_to_dict
+from publisher.pubmodels.message_model import MessageModel
+from subscribers.models import SubscriberModel
+import requests
+
+class JsonService:
+    @staticmethod
+    def send(instance:MessageModel, url:SubscriberModel):
+        json = model_to_dict(instance)
+        r = requests.post(url= SubscriberModel.webhook, data=json)
+        return r
--- PubSub/core/tasks/message_dispacher_task.py
+++ PubSub/core/tasks/message_dispacher_task.py
@@ -4,14 +4,21 @@ from django_q.tasks import async_task
 from publisher.pubmodels.message_model import MessageModel
 from subscribers.models import SubscriberModel
 from core.services.email_service import EmailService
+from core.services.http_service import JsonService
+
     
 def message_dispacher(sender,instance: MessageModel,created:bool, **kwargs)->None:
     if created:
         subs = SubscriberModel.objects.filter(servicePub=instance.servicePub)
+        listofemails = [] 
         for i in subs:
             if i.webhook:
                 #TODO: pasarlo a un servicio
-                async_task('requests.post', i.webhook,  json=model_to_dict(instance))
+                async_task('core.services.http_service.JsonService.send')
             if i.email:
-                #TODO: hacer un acumolador
-                async_task('core.services.email_service.EmailService.send', instance, [i.email])
+                listofemails.append(i.email)
+                if len(listofemails) == 10:
+                    for mails in listofemails:
+                        async_task('core.services.email_service.EmailService.send', instance, [mails])
+                    listofemails.clear()
+                    
