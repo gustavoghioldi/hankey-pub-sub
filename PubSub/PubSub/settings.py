d516be8510a5cf7fc67d76aafcb68e924e96da5d
Leandro090685 <lenife@hotmail.com>
2022-12-06 18:50:22 -0300

    os

--- PubSub/PubSub/settings.py
+++ PubSub/PubSub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -173,5 +174,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ.get["EMAIL_HOST_PASSWORD"]
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
